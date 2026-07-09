cac67cafd6658c41427544d211bbdb7f1fe2f94b
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-09 12:06:08 +0530

    Time: 6 ms (51.42%), Space: 65.1 MB (69.63%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        set<int>st;
-        for(int i:nums){
-            if(st.count(i))    return i;
-            st.insert(i);
+        int slow=nums[0],fast=nums[0];
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast)  break;
         }
-        return -1;
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
     }
 };
