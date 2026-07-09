978a46aa1395b4cd487115143b41b920df2a14d1
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-09 10:12:34 +0530

    Time: 0 ms (100%), Space: 12.2 MB (97.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
+    void swapifgreater(vector<int>& nums1, int idx1, vector<int>& nums2,int idx2){
+        if(nums1[idx1]>nums2[idx2]) swap(nums1[idx1],nums2[idx2]);
+    }
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left=m-1,right=0;
-        while(left>=0 && right<n){
-            if(nums1[left]>nums2[right]){
-                swap(nums1[left],nums2[right]);
-                left--,right++;
+        int len=(m+n);
+        int gap=(len/2)+(len%2);
+        while(gap>0){
+            int left=0;
+            int right=left+gap;
+            while(right<len){
+                if(left<m && right>=m){
+                    swapifgreater(nums1,left,nums2,right-m);
+                }    
+                else if(left>=m){
+                    swapifgreater(nums2,left-m,nums2,right-m);
+                }           
+                else{
+                    swapifgreater(nums1,left,nums1,right);
+                }
+                left++,right++;
             }
-            else{
-                break;
-            }
+            if(gap==1)  break;
+            gap=(gap/2)+(gap%2);
         }
-        sort(nums1.begin(),nums1.begin()+m);
-        sort(nums2.begin(),nums2.end());
         for(int i=0;i<n;i++){
             nums1[m+i]=nums2[i];
         }
