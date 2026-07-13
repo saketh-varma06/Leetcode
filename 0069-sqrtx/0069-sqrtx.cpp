f6fe4b6ba7094e38ebd9a05e3f78bcb39c948640
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-13 10:13:32 +0530

    Time: 1 ms (21.08%), Space: 8.5 MB (49.8%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=1,h=x;
+        while(l<=h){
+            long long mid=l+(h-l)/2;
+            long long val=(mid*mid);
+            if(val<=x){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return h;
+    }
+};
