c702d9c2ee8260dc71b587e806a7dc1d45d7c015
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-18 16:16:16 +0530

    Time: 26 ms (57.51%), Space: 88.2 MB (96.96%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    bool candiesweallocate(vector<int>& candies,int mid,long long k){
-        long long cnt=0;
-        for(int i=0;i<candies.size();i++){
-            cnt+=(candies[i]/mid);
-            if(cnt>=k)   return true;
+    bool candiesweallocate(vector<int>& candies, int mid, long long k) {
+
+        long long cnt = 0;
+
+        for (int x : candies) {
+            cnt += x / mid;
+
+            if (cnt >= k)
+                return true;
         }
+
         return false;
     }
+
     int maximumCandies(vector<int>& candies, long long k) {
-        if(accumulate(candies.begin(),candies.end(),0LL)<k)   return 0;
-        int l=1,h=*max_element(candies.begin(),candies.end());
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(candiesweallocate(candies,mid,k)){
-                l=mid+1;
-            }
-            else{
-                h=mid-1;
-            }
+
+        if (accumulate(candies.begin(), candies.end(), 0LL) < k)
+            return 0;
+
+        int l = 1;
+        int h = *max_element(candies.begin(), candies.end());
+
+        while (l <= h) {
+
+            int mid = l + (h - l) / 2;
+
+            if (candiesweallocate(candies, mid, k))
+                l = mid + 1;
+            else
+                h = mid - 1;
         }
+
         return h;
     }
 };
