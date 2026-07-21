694c8deae2a290ff9b08b3df6bd395076eb27c6d
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-21 15:38:31 +0530

    Time: 56 ms (53.47%), Space: 151 MB (64.47%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,51 +1,30 @@
 class MinStack {
 public:
-    stack<long long>st;
-    long long mini=1e9;
+    stack<pair<int,int>> st;
     MinStack() {
         
     }
     
     void push(int value) {
         if(st.empty()){
-            mini=value;
-            st.push(value);
+            st.push({value,value});
         }
         else{
-            if(value>mini){
-                st.push(value);
-            }
-            else{
-                st.push(2LL*value-mini);
-                mini=value;
-            }
+            st.push({value,min(value,st.top().second)});
         }
     }
     
     void pop() {
-        if(st.empty()){
-            return;
-        }
-        long long x=st.top();
+        if(st.empty())  return;
         st.pop();
-        if(x<mini){
-            mini=2*mini-x;
-        }
     }
     
     int top() {
-        if(st.empty()){
-            return -1;
-        }
-        long long x=st.top();
-        if(mini<x){
-            return x;
-        }
-        return mini;
+        return st.top().first;
     }
     
     int getMin() {
-        return mini;
+        return st.top().second;
     }
 };
 
