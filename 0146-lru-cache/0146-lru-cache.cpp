b03e9448de05b8196c9ed30e4ef9d3d2a7cb0d11
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-24 11:56:14 +0530

    Time: 72 ms (64.83%), Space: 173.1 MB (81.64%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,64 +1,65 @@
 class LRUCache {
 public:
-    class node{
+    class node {
     public:
-        int key,value;
-        node *next,*prev;
-        node(int _key,int _value){
-            key=_key;
-            value=_value;
+        int key, value;
+        node *next, *prev;
+        node(int _key, int _value) {
+            key = _key;
+            value = _value;
+            next = nullptr;
+            prev = nullptr;
         }
     };
-    node *head=new node(-1,-1);
-    node *tail=new node(-1,-1);
+    node *head = new node(-1, -1);
+    node *tail = new node(-1, -1);
     int cap;
-    unordered_map<int,node*>mpp;
+    unordered_map<int, node*> mpp;
     LRUCache(int capacity) {
-        cap=capacity;
-        head->next=tail;
-        tail->prev=head;
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;
     }
-    void addnode(node* newnode){
-        node *temp=head->next;
-        newnode ->next=temp;
-        newnode->prev=head;
-        head->next=newnode;
-        temp->prev=newnode;
+    void addnode(node *newnode) {
+        node *temp = head->next;
+        newnode->next = temp;
+        newnode->prev = head;
+        head->next = newnode;
+        temp->prev = newnode;
     }
-    void deletenode(node *delnode){
-        node *delprev=delnode->prev;
-        node *delnext=delnode->next;
-        delprev->next=delnext;
-        delnext->prev=delprev;
+    void deletenode(node *delnode) {
+        node *delprev = delnode->prev;
+        node *delnext = delnode->next;
+        delprev->next = delnext;
+        delnext->prev = delprev;
     }
     int get(int key_) {
-        if(mpp.find(key_)==mpp.end())   return -1;
-        node *resnode=mpp[key_];
+        if (mpp.find(key_) == mpp.end())
+            return -1;
+        node *resnode = mpp[key_];
         deletenode(resnode);
         addnode(resnode);
         return resnode->value;
     }
-    
     void put(int key_, int value) {
-        if(mpp.find(key_)!=mpp.end()){
-            node *existingnode=mpp[key_];
-            existingnode->value=value;
+        if (mpp.find(key_) != mpp.end()) {
+            node *existingnode = mpp[key_];
+            existingnode->value = value;
             deletenode(existingnode);
             addnode(existingnode);
             return;
         }
-        if(mpp.size()==cap){
-            node*node=tail->prev;
-            mpp.erase(node->key);
-            deletenode(node);
-            delete node;
+        if (mpp.size() == cap) {
+            node *lru = tail->prev;
+            mpp.erase(lru->key);
+            deletenode(lru);
+            delete lru;
         }
-        node *newnode=new node(key_,value);
+        node *newnode = new node(key_, value);
         addnode(newnode);
-        mpp[key_]=newnode;
+        mpp[key_] = newnode;
     }
 };
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
