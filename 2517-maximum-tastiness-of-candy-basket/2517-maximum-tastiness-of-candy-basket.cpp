f9654553e35c37b880ebbca0b24b05e18da7dd45
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-07-28 16:38:00 +0530

    Time: 55 ms (26.28%), Space: 52.2 MB (92.84%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool possible(vector<int>& price,int k,int mid){
+        int cnt=1,last=price[0];
+        for(int i=1;i<price.size();i++){
+            if(price[i]-last>=mid){
+                cnt++;
+                last=price[i];
+            }
+        }
+        return cnt>=k;
+
+    }
+    int maximumTastiness(vector<int>& price, int k) {
+        sort(price.begin(),price.end());
+        int n=price.size();
+        int l=1,h=price[n-1]-price[0];
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(possible(price,k,mid)){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return h;
+    }
+};
