6eb1b85c05fe5a8121d4b15ff568b12d7f0289bb
saketh varma kandakatla <2403a510b6@sru.edu.in>
2026-09-20 11:03:38 +0530

    Time: 4 ms (21.48%), Space: 52.3 MB (57.37%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_idx=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>max_idx)   return false;
+            max_idx=max(max_idx,nums[i]+i);
+        }
+        return true;
+    }
+};
